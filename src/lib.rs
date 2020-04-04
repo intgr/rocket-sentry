7d722e38d18172772b856b6b59b4fe0cd396ae35
Marti Raudsepp <marti@juffo.org>
2020-04-04 17:41:18 +0300

    Add badges & copy documentation to lib.rs (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+#![allow(clippy::needless_doctest_main)]
+//! **rocket-sentry** is a simple add-on for the **Rocket** web framework to simplify
+//! integration with the **Sentry** application monitoring system.
+//!
+//! Or maybe...
+//!
+//! > "The Rocket Sentry is a static rocket-firing gun platform that is based on a
+//! > Personality Construct and used in the Aperture Science Enrichment Center."
+//! >
+//! > -- [Half-Life wiki](https://half-life.fandom.com/wiki/Rocket_Sentry)
+//!
+//! Example usage
+//! =============
+//!
+//! ```no_run
+//! use rocket_sentry::RocketSentry;
+//!
+//! fn main() {
+//!     rocket::ignite()
+//!         .attach(RocketSentry::fairing())
+//!         // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   add this line
+//!         .launch();
+//! }
+//! ```
+//!
+//! Then, the Sentry integration can be enabled by adding a `sentry_dsn=` value to
+//! the `Rocket.toml` file, for example:
+//!
+//! ```toml
+//! [development]
+//! sentry_dsn = ""  # Disabled
+//! [staging]
+//! sentry_dsn = "https://057006d7dfe5fff0fbed461cfca5f757@sentry.io/1111111"
+//! [production]
+//! sentry_dsn = "https://057006d7dfe5fff0fbed461cfca5f757@sentry.io/1111111"
+//! ```
+//!
 use std::sync::Mutex;
 
 use rocket::fairing::{Fairing, Info, Kind};
